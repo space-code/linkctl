be74596d9598acc53bcc4db50955898d5716cc7a
Nikita Vasilev <nv3212@gmail.com>
2026-03-05 15:04:13 +0400

    feat: add colored output for console logs (#16)

--- pkg/iostreams/iostreams.go
+++ pkg/iostreams/iostreams.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"io"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 type fileWriter interface {
@@ -20,6 +22,8 @@ type IOStreams struct {
 	In     fileReader
 	Out    fileWriter
 	ErrOut fileWriter
+
+	colorEnabled bool
 }
 
 func System() *IOStreams {
@@ -28,6 +32,8 @@ func System() *IOStreams {
 		Out:    os.Stdout,
 		ErrOut: os.Stderr,
 	}
+
+	ios.colorEnabled = !color.NoColor
 
 	return ios
 }
@@ -42,11 +48,55 @@ func Test() (*IOStreams, *bytes.Buffer, *bytes.Buffer, *bytes.Buffer) {
 			fd:         0,
 			ReadCloser: io.NopCloser(in),
 		},
-		Out:    &fdWriter{fd: 1, Writer: out},
-		ErrOut: &fdWriter{fd: 2, Writer: errOut},
+		Out:          &fdWriter{fd: 1, Writer: out},
+		ErrOut:       &fdWriter{fd: 2, Writer: errOut},
+		colorEnabled: false,
 	}
 
 	return io, in, out, errOut
+}
+
+func (ios *IOStreams) ColorEnabled() bool {
+	return ios.colorEnabled
+}
+
+type ColorScheme struct {
+	enabled bool
+}
+
+func (ios *IOStreams) ColorScheme() *ColorScheme {
+	return &ColorScheme{enabled: ios.colorEnabled}
+}
+
+func (cs *ColorScheme) format(c *color.Color, s string) string {
+	if !cs.enabled {
+		return s
+	}
+	return c.Sprint(s)
+}
+
+func (cs *ColorScheme) Bold(s string) string {
+	return cs.format(color.New(color.Bold), s)
+}
+
+func (cs *ColorScheme) Green(s string) string {
+	return cs.format(color.New(color.FgGreen), s)
+}
+
+func (cs *ColorScheme) Cyan(s string) string {
+	return cs.format(color.New(color.FgCyan), s)
+}
+
+func (cs *ColorScheme) Yellow(s string) string {
+	return cs.format(color.New(color.FgYellow), s)
+}
+
+func (cs *ColorScheme) Red(s string) string {
+	return cs.format(color.New(color.FgRed), s)
+}
+
+func (cs *ColorScheme) Muted(s string) string {
+	return cs.format(color.New(color.FgHiBlack), s)
 }
 
 type fdWriter struct {
